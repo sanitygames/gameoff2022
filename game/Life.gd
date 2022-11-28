93c53f20895806eb03f8a6c87f33b93ba843891e
sayamapp <sayamapp@gmail.com>
2022-11-28 18:23:23 +0900

    life.gd changed

--- game/Life.gd
+++ game/Life.gd
@@ -11,29 +11,3 @@ func damage():
 		emit_signal("game_over")
 	else:
 		SoundManager.get_node("Damage").play()
-
-# var life = 3
-# var sprites = []
-
-# func _ready():
-# 	for child in get_children():
-# 		if child is Sprite:
-# 			sprites.push_back(child)
-
-
-# func damage():
-# 	if life <= 0:
-# 		return
-
-# 	life -= 1
-# 	var s = sprites.pop_back()
-# 	s.visible = false
-# 	if life <= 0:
-# 		$GameOver.play()
-# 		emit_signal("game_over")
-# 	else:
-# 		$Damage.play()
-
-
-# func _on_Button_pressed():
-# 	damage()
