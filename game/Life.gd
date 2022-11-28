970d03c8859fdad3e918aff3e27cf1b319034e5c
sayamapp <sayamapp@gmail.com>
2022-11-28 18:28:25 +0900

    weapon.gd changed

--- game/Life.gd
+++ game/Life.gd
@@ -5,7 +5,6 @@ signal game_over
 func damage():
 	var _children = get_children()
 	var _c = _children.pop_back().queue_free()
-	print(_c)
 	if _children.size() == 0:
 		SoundManager.get_node("GameOver").play()
 		emit_signal("game_over")
--- weapon/Weapon.gd
+++ weapon/Weapon.gd
@@ -1,19 +1,11 @@
 extends KinematicBody2D
 
-
-var velocity = Vector2.ZERO setget set_velocity
-var g = 0.0 setget set_gravity
-
-func set_velocity(value):
-	velocity = value
-
-func set_gravity(value):
-	g = value
+var velocity = Vector2.ZERO
+var g = 0.0
 
 func _physics_process(delta):
 	velocity.y += g * delta
 	var _c = move_and_collide(velocity * delta)
-
 
 func _on_Timer_timeout():
 	queue_free()
